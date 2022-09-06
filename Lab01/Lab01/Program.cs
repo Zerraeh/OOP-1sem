ab18bd1851678b4d5014c285cd91282ac356fe86
Zerraeh <sashasakovich9@gmail.com>
2022-09-06 08:50:41 +0300

    added 1-a

---
+++ Lab01/Lab01/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Lab01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1-a
+            bool booleanValue = true;
+            byte byteValue = 22;
+            sbyte sbyteValue = -2;
+            char charValue = 'a';
+            decimal decimalValue = 22.5m;
+            double doubleValue = 22.5;
+            float floatValue = 22.5f;
+            int intValue = -2;
+            uint uintValue = 1;
+            nint nintValue = -7;
+            nuint nuintValue = 1;
+            long longValue = -5;
+            ulong ulongValue = 3;
+            short shortValue = -3;
+            ushort ushortValue = 3;
+            Console.WriteLine(booleanValue + "\t" + byteValue + "\t" + sbyteValue + "\t" + charValue + "\t" + decimalValue + "\t" + doubleValue +
+                "\t" + floatValue + "\t" + intValue + "\t" + uintValue + "\t" + nintValue + "\t" + nuintValue + "\t" + longValue + "\t" + ulongValue + "\t" +
+                shortValue + "\t" + ushortValue);
+        }
+    }
+}
