70553658d6c181362f2f1d5d6b719a0711e4699f
Zerraeh <sashasakovich9@gmail.com>
2022-10-11 14:21:16 +0300

    added Exception class TransportExceptions and created some classes based on it.

--- Lab06/Lab06/Exceptions.cs
+++ Lab06/Lab06/Exceptions.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace Lab06
 {
-    internal class Exceptions
+    public class TransportExceptions : Exception
     {
+        public TransportExceptions(string message, string error) : base(message)
+        {
+            Error = error;
+        }
+        public string Error { get; }
+    }
+    public class CarFuelConsumeExceptions : TransportExceptions
+    {
+        public CarFuelConsumeExceptions(string message, string error) : base(message, "carFuelConsume")
+        {
+            
+        }
+        public int fuelConsume { get; private set; }
+    }
+
+    public class TrainSpeedExceptions : TransportExceptions
+    {
+        public TrainSpeedExceptions(string message, string error) : base(message, "trainSpeed")
+        {
+
+        }
+        public int trainSpeed { get; private set; }
     }
 }
