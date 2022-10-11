1665c71908f549913b51a39b9cc95c14ca4d3c19
Zerraeh <sashasakovich9@gmail.com>
2022-10-11 14:23:43 +0300

    partials.cs fixed

--- Lab06/Lab06/partials.cs
+++ Lab06/Lab06/partials.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static Lab06.Program.Program;
+using static Lab06.Program;
 
 namespace Lab06
 {
