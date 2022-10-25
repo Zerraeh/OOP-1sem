efea7f79752ac0ea300da3bfa18bd663d616273e
Zerraeh <sashasakovich9@gmail.com>
2022-10-25 12:05:06 +0300

    added find by value for second collection + sorted all code

--- Lab09/Lab09/Program.cs
+++ Lab09/Lab09/Program.cs
@@ -2,8 +2,8 @@
 {
     internal class Program
     {
-
-        static void Main(string[] args)
+        #region Часть первого задания
+        public static void First()
         {
             GeometricFigure[] geometricFigures = new GeometricFigure[3]
             {
@@ -16,14 +16,19 @@
             {
                 Console.WriteLine("Длина: " + g.Length + "\n Ширина: " + g.Width + "\n---\n");
             }
-            geometricFigureList.Find(7,3);
+            geometricFigureList.Find(7, 3);
             geometricFigureList.Delete();
             foreach (GeometricFigure g in geometricFigureList)
             {
                 Console.WriteLine("Длина: " + g.Length + "\n Ширина: " + g.Width + "\n---\n");
             }
+        }
+        #endregion
 
 
+        #region Второе задание
+        public static void Second()
+        {
             //2 задание
             /*
                 +Создайте универсальную коллекцию в соответствии с вариантом задания и 
@@ -38,17 +43,17 @@
                 +количества параметров (например, LinkedList<T> и Dictionary<Tkey, 
                 +TValue>), при нехватке - генерируйте ключи, в случае избыточности –
                 +оставляйте TValue.
-                f. Найдите во второй коллекции заданное значение
+                +f. Найдите во второй коллекции заданное значение
             */
             var firstCollection = new Stack<int>();
             var tempCollection = new Stack<int>();
             Random random = new Random();
-            int Size = 5;
+            int Size = 10;
 
             //заполняем первую коллекцию
             for (int i = 0; i < Size; i++)
             {
-                firstCollection.Push(random.Next(i,10));
+                firstCollection.Push(random.Next(i, 10));
             }
             //выводим первую коллекцию
             Console.WriteLine("\n\nВывод первой коллекции: ");
@@ -90,6 +95,44 @@
                 secondTempCollection.Enqueue(temp);
                 Console.WriteLine($"{i}\t-\t{temp}");
             }
+
+            //нахождение элемента внутри второй коллекции
+            Console.WriteLine("Найденный элемент: ");
+            int findValue = 4;
+            for (int i = 0; i < Size; i++)
+            {
+                int temp;
+                temp = secondTempCollection.Dequeue();
+                secondCollection.Enqueue(temp);
+                if (temp == findValue)
+                {
+                    Console.WriteLine($"Элемент {findValue} существует во второй коллекции");
+                }
+            }
         }
+        #endregion
+
+
+        #region Третье задание
+        public static void Third()
+        {
+
+        }
+        #endregion
+        static void Main(string[] args)
+        {
+            int n = 1;
+            Console.WriteLine($"\n\t--- Выполнение {n} задания: ");
+            First();
+            n++;
+
+            Console.WriteLine($"\n\t--- Выполнение {n} задания: ");
+            Second();
+            n++;
+
+            Console.WriteLine($"\n\t--- Выполнение {n} задания: ");
+            Third();
+
+        } 
     }
 }
