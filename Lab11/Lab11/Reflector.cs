7305354b0a3dab44abd968f4bce0b44f0553cd64
Zerraeh <sashasakovich9@gmail.com>
2022-11-21 18:35:35 +0300

    added GetAssemblyName function

--- Lab11/Lab11/Reflector.cs
+++ Lab11/Lab11/Reflector.cs
@@ -6,8 +6,20 @@ using System.Threading.Tasks;
 
 namespace Lab11
 {
-    internal class Reflector
+    public static class Reflector
     {
+        private static StreamWriter? fileForInformation = null;
+        private static StreamReader? fileForInvoke = null;
 
+
+        private static readonly string fileForInformationPath = @"D:\_work\ООП\1sem\Lab11\Lab11\Reflection.txt";
+        private static readonly string fileForInvokePath = @"D:\_work\ООП\1sem\Lab11\Lab11";
+
+        // a - определение имени сборки, в которой определён класс
+        public static string GetAssemblyName(Type CurrentClass)
+        {
+            return CurrentClass.Assembly.ToString();
+        }
+        
     }
 }
