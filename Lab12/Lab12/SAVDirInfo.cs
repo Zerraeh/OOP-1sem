0b07e032b0302d1932f9d8f165c0dc58e51774d8
Zerraeh <sashasakovich9@gmail.com>
2022-11-27 20:23:43 +0300

    all SAVDirInfo methods

---
+++ Lab12/Lab12/SAVDirInfo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab12
+{
+    /*Создать класс XXXDirInfo c методами для вывода информации о конкретном
+директории
+a.Количестве файлов
+b. Время создания
+c.Количестве поддиректориев
+d.Список родительских директориев
+e. Продемонстрируйте работу класс*/
+    public static class SAVDirInfo
+    {
+        public static void NumberOfFilesShow(string path)
+        {
+            Console.WriteLine($"Число файлов:\t{Directory.GetFiles(path).Length}");
+        }
+
+        public static void CreationTimeShow(string path)
+        {
+            Console.WriteLine($"Время создания:\t{Directory.GetCreationTime(path)}");
+        }
+
+        public static void NumberOfSubdirectoriesShow(string path)
+        {
+            Console.WriteLine($"Число поддиректорий:\t{Directory.GetDirectories(path).Length}");
+        }
+
+        public static void ParentDirShow(string path)
+        {
+            Console.WriteLine($"Список родительских директорий:\t{Directory.GetParent(path)}");
+        }
+    }
+}
