99180fead9616cd1a55c176d78a91f1bebd14899
Zerraeh <sashasakovich9@gmail.com>
2022-11-27 20:03:36 +0300

    added FileSystemInfoShow method

--- Lab12/Lab12/SAVDiskInfo.cs
+++ Lab12/Lab12/SAVDiskInfo.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace Lab12
 {
-    internal class SAVDiskInfo
+    public static class SAVDiskInfo
     {
+        /*2. Создать класс XXXDiskInfo c методами для вывода информации о
+a.свободном месте на диске
+b. Файловой системе
+c.Для каждого существующего диска - имя, объем, доступный
+объем, метка тома.
+d.Продемонстрируйте работу класса*/
+
+        public static void FreeSpaceShow(string disk)
+        {
+            var currentDisk = DriveInfo.GetDrives().Single(x=> x.Name == disk);
+            Console.WriteLine($"Свободное место на диске {disk}: {currentDisk.AvailableFreeSpace} байт...");
+        }
+
+        public static void FileSystemInfoShow(string disk)
+        {
+            var currentDisk = DriveInfo.GetDrives().Single(x => x.Name == disk);
+            Console.WriteLine($"Информация о файловой системе диска {disk} : {currentDisk.DriveType}\t-\t{currentDisk.DriveFormat}");
+        }
+
+
     }
 }
