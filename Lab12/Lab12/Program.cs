45fe690650474e7e1f2e97ba01d04cd3051a13a4
Zerraeh <sashasakovich9@gmail.com>
2022-11-27 19:54:48 +0300

    added SAVLog with WriteLog() method

---
+++ Lab12/Lab12/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Lab12
+{
+    
+    internal class Program
+    {
+
+        
+
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
---
+++ Lab12/Lab12/SAVLog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Lab12
+{
+    public static class SAVLog
+    {
+        public static void WriteLog(string message)
+        {
+            using (var stream = new StreamWriter(@"D:\_work\ООП\1sem\Lab12\Lab12"))
+            {
+                stream.WriteLine($"{DateTime.Now.ToString()}\t-\t{message}");
+            }
+        }
+            /*Создать класс XXXLog.Он должен отвечать за работу с текстовым файлом
+xxxlogfile.txt.в который записываются все действия пользователя и
+соответственно методами записи в текстовый файл, чтения, поиска нужной
+информации*/
+    }
+}
